24a2540696c6c43cd7000157c971b7c55d3368f1
Aditya Dobhada <adisb6262@gmail.com>
2024-07-06 12:18:39 +0530

    Initialized the handlers for user signup and signin, creating and updating the blog

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,26 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono().basePath('/api/v1')
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/signup', (c) => {
+  return c.text("signup route")
 })
+
+app.post('/signin', (c) => {
+  return c.text("signin route")
+})
+
+app.get('/blog/:id', (c) => {
+  return c.text("get blog route")
+})
+
+app.post('/blog', (c) => {
+  return c.text("create blog route")
+})
+
+app.put('/blog', (c) => {
+  return c.text("update blog route")
+} )
 
 export default app
