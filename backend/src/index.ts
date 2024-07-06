cc50aaa95145447c087bb7debff96cff54bb1c2b
Aditya Dobhada <adisb6262@gmail.com>
2024-07-06 15:30:36 +0530

    Initialized the prisma client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,33 @@
-import { Hono } from 'hono'
+import { Env, Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono().basePath('/api/v1')
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>().basePath('/api/v1')
+
+app.post('/signup', async (c) => {
+
+  const prisma = new PrismaClient({
+  datasourceUrl:c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    }
+  })
 
 
-app.post('/signup', (c) => {
   return c.text("signup route")
 })
+
 
 app.post('/signin', (c) => {
   return c.text("signin route")
