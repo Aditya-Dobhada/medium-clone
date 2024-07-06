8afd756d5e7a35f342bf0399b757fc9f3936a1a2
Aditya Dobhada <adisb6262@gmail.com>
2024-07-06 15:52:30 +0530

    Used bcrypt.ts to hash passwords

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Env, Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import bcrypt from 'bcrypt-ts'
 
 const app = new Hono<{
   Bindings: {
@@ -15,15 +15,16 @@ app.post('/signup', async (c) => {
   datasourceUrl:c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
+  const salt = bcrypt.genSaltSync(10)
+
   const body = await c.req.json();
 
   await prisma.user.create({
     data: {
       email: body.email,
-      password: body.password,
+      password: bcrypt.hashSync(body.password, salt),
     }
   })
-
 
   return c.text("signup route")
 })
