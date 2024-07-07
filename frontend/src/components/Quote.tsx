ae8217de5bff08ea102081fde26966ff7f4e09ab
Aditya Dobhada <adisb6262@gmail.com>
2024-07-08 00:58:51 +0530

    wrote the logic for signup and signin pages

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+export const Quote = () => {
+    return (
+      <div className="bg-slate-200 h-screen flex justify-center ">
+        <div className="grid content-center font-semibold text-3xl">
+          <div className="px-4">
+            "The customer service I received was exceptional. The support team
+            went above and beyond to address my concerns"
+            <div className="pt-2">
+              <div className="font-semibold text-lg">Jules Winnfield</div>
+
+              <div className="font-light text-slate-500 text-sm">CEO, Acme Inc</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
