0a2ddb8b95c6deb001320f7be5d09bf15ed663de
Aditya Dobhada <adisb6262@gmail.com>
2024-07-08 03:22:11 +0530

    Added all the updated routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from './pages/Publish';
 
 function App() {
-
-
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
